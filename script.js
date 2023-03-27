69796cc693431195059ef709e4722f2c16aeef78
michaeltrinh <asdf.michael@hotmail.com>
2023-03-27 17:26:19 +1100

    added func for key input

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     createSquares();
 
+    const keys = document.querySelectorAll('.keyboard-row button')
+
+    function handleClick(event) {
+      const target = event.target;
+      const key = target.getAttribute('data-key');
+      console.log(key);
+    }
+    for (let i = 0; i < keys.length; i++) {
+       keys[i].onclick = handleClick;
+    }
+
+
     function createSquares() {
         const gridBoard = document.getElementById('grid');
 
@@ -11,4 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
             gridBoard.appendChild(squares);
         }
     }
+
+
+
 });
