6499dc825752f34875f20351afec1e74bbacc11b
michaeltrinh <asdf.michael@hotmail.com>
2023-03-30 20:17:34 +1100

    had alot of trouble getting the word in the correct position to turn green, vishal was able to guide me and eventually got there

--- script.js
+++ script.js
@@ -1,104 +1,122 @@
-document.addEventListener('DOMContentLoaded', function() {
-    createSquares();
 
-    let guessedWords = [[]]
-    let availableSpace = 1
+createSquares();
 
-    let word = 'HELLO'
+let squareCounter = 0
 
-    const keys = document.querySelectorAll('.keyboard-row')
+let guessedWords = [[]]
+let availableSpace = 1
 
+let word = 'HELLO'
 
-    function getCurrentWordArray() {
-        const numberOfGuessedWords = guessedWords.length;
-        return guessedWords[numberOfGuessedWords - 1];
-      }
+const keys = document.querySelectorAll('.keyboard-row')
 
 
+function getCurrentWordArray() {
+    const numberOfGuessedWords = guessedWords.length;
+    return guessedWords[numberOfGuessedWords - 1];
+}
 
-    function updateGuessedWord(letter) {
-        const currentWordArray = getCurrentWordArray()
 
-        if (currentWordArray && currentWordArray.length < 5) {
-            currentWordArray.push(letter)
 
-            let availableSpaceElement = document.getElementById(availableSpace)
 
-            availableSpace = availableSpace + 1
-            availableSpaceElement.textContent = letter
-        }
+
+function updateGuessedWord(letter) {
+    const currentWordArray = getCurrentWordArray()
+
+    if (currentWordArray && currentWordArray.length < 5) {
+        currentWordArray.push(letter)
+        // console.log(guessedWords);
+        // console.log(squareCounter);
+        let availableSpaceElement = document.getElementById(availableSpace)
+
+        availableSpace = availableSpace + 1
+        availableSpaceElement.textContent = letter
+        squareCounter++
+
+                // Check if the letter is in the correct position
+                const position = currentWordArray.length - 1
+                if (word[position] === letter) {
+                    // If the letter is in the correct position, add a CSS class to the square element
+                    const squareElement = document.getElementById(squareCounter)
+                    squareElement.classList.add('correct')
+                } 
+    }
+}
+
+
+
+
+
+function handleSubmitWord() {
+    const currentWordArray = getCurrentWordArray()
+    if (currentWordArray.length !==5) {
+        alert ('Must be 5 letters')
+        return
+    }
+
+    const currentWord = currentWordArray.join('')
+
+    if (currentWord === word) {
+        alert ('You Win!')
     }
 
 
-
-    function handleSubmitWord() {
-        const currentWordArray = getCurrentWordArray()
-        if (currentWordArray.length !==5) {
-            alert ('Must be 5 letters')
-            return
-        }
-
-        const currentWord = currentWordArray.join('')
-
-        if (currentWord === word) {
-            alert ('You Win!')
-        }
-
-
-        if (guessedWords.length === 6) {
-            alert (`No more guesses! The word is ${word}. Refresh page to play again`)
-        }
-
-        guessedWords.push([])
+    if (guessedWords.length === 6) {
+        alert (`No more guesses! The word is ${word}. Refresh page to play again`)
     }
 
+    guessedWords.push([])
+}
 
 
-    function createSquares() {
-        const gridBoard = document.getElementById('grid');
 
-        for (let index = 0; index < 30; index++) {
-            let squares = document.createElement('div');
-            squares.classList.add('squares');
-            squares.setAttribute('id', index + 1);
-            gridBoard.appendChild(squares);
-        }
+
+
+function createSquares() {
+    const gridBoard = document.getElementById('grid');
+
+    for (let index = 0; index < 30; index++) {
+        let squares = document.createElement('div');
+        squares.classList.add('squares');
+        squares.setAttribute('id', index + 1);
+        gridBoard.appendChild(squares);
+    }
+}
+
+
+
+
+function handleDeleteLetter() {
+    const currentWordArray = getCurrentWordArray()
+    const removeLetter = currentWordArray.pop
+
+    guessedWords[guessedWords.length - 1] = currentWordArray
+
+    const lastLetterElement = document.getElementById(availableSpace - 1)
+
+    lastLetterElement.textContent = ''
+    availableSpace = availableSpace - 1
+}
+
+
+
+
+
+function handleClick(event) {
+    const letter = event.target.dataset.key;
+
+    if (letter === 'enter') {
+        handleSubmitWord()
+        return
     }
 
-
-    function handleDeleteLetter() {
-        const currentWordArray = getCurrentWordArray()
-        const removeLetter = currentWordArray.pop
-
-        guessedWords[guessedWords.length - 1] = currentWordArray
-
-        const lastLetterElement = document.getElementById(availableSpace - 1)
-
-        lastLetterElement.textContent = ''
-        availableSpace = availableSpace - 1
+    if (letter === 'delete') {
+       handleDeleteLetter()
+      return
     }
 
-
-
-    function handleClick(event) {
-        const letter = event.target.dataset.key;
-
-        if (letter === 'enter') {
-            handleSubmitWord()
-            return
-        }
-
-        if (letter === 'delete') {
-            handleDeleteLetter()
-            return
-        }
-
-        updateGuessedWord(letter);
-      }
-      for (let i = 0; i < keys.length; i++) {
-         keys[i].onclick = handleClick;
-      }
-
-
-
-}); 
+    updateGuessedWord(letter);
+    }
+    for (let i = 0; i < keys.length; i++) {
+    keys[i].onclick = handleClick;
+}
